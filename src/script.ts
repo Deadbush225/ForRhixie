94b8fa810d09d081ad2180d06cb7fa4d4962d215
Eliazar Inso <eliazar225ninso@gmail.com>
2023-12-21 22:35:44 +0800

    feat: Added picture randomizer for flickity

--- src/script.ts
+++ src/script.ts
@@ -18,6 +18,8 @@ import "../node_modules/flickity/dist/flickity.min.css";
 
 import "./styles/styles.scss";
 
+import TypeIt from "typeit";
+
 function sleepFor(sleepDuration: number) {
 	var now = new Date().getTime();
 	while (new Date().getTime() < now + sleepDuration) {
@@ -25,26 +27,34 @@ function sleepFor(sleepDuration: number) {
 	}
 }
 
-let num = 1;
+let range = Array.from({ length: 54 }, (_, i) => i + 1);
+// console.log(range);
+
+// let num = 1;
 for (let index = 0; index < 6; index++) {
 	let $gallery = $([`<div class="carousel">`, `</div>`].join("\n"));
 
 	// let $gallery = $(".carousel");
 
 	for (let index = 1; index < 10; index++) {
+		let rand_index = Math.floor(Math.random() * range.length);
+		console.log(rand_index);
+		console.log(range[rand_index]);
+		// index = rand * range.length; //(5)
 		$gallery.append(
 			'<div class="carousel-cell"><img id="image" src="./assets/img(' +
-				num +
+				range[rand_index] +
 				').jpg" alt="" /></div>'
 		);
-		num++;
+		range.splice(rand_index, 1);
+		// num++;
 	}
 
 	$(".gallery-container").append($gallery);
 }
 
 $("body").waitForImages(function () {
-	console.log("Images have been loaded");
+	// console.log("Images have been loaded");
 	$(".carousel").each(function (index, element) {
 		console.log(element);
 		let odd_or_even = index % 2 == 0;
@@ -61,6 +71,7 @@ $("body").waitForImages(function () {
 			prevNextButtons: false,
 			pageDots: false,
 			pauseAutoPlayOnHover: false,
+			cellAlign: "left",
 		});
 	});
 });
